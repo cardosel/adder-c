cca87c6fad3bb33c6d47449019bdfc5025d6a8b0
Selena Cardona <cardo23s@mtholyoke.edu>
2016-02-10 15:42:44 -0500

    adder

---
+++ adding.c
@@ -0,0 +1,18 @@
+#include <stdio.h> //C version of an import statement
+
+int main(void) {
+
+	//declare a variable called x and intialize it to 221
+	int x = 221;
+	int y = 200;
+	int sum = 0;
+
+	sum = x + y;
+
+
+	// C version of System.out.println
+	printf("Sum of entered numbers = %d\n" , sum);
+
+	return 0;
+}
+
